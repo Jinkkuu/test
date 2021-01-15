9f217d8e4df7798daf36d17b5c067efc7be1f68a
Mojeg <49614252+mojeg@users.noreply.github.com>
2021-01-14 19:00:09 -0500

    Add files via upload

---
+++ chk.py
@@ -0,0 +1,29 @@
+import os
+#print('Denix Sandbox')
+appversion=2.0
+appname='Updater'
+space=' '
+appcversion=2
+
+
+
+
+
+
+print(str(appname)+str(space)+str(appversion))
+exit()
+print('Checking Version')
+os.system('wget https://raw.githubusercontent.com/mojeg/test/main/versionchk -q')
+f=open('versionchk','r')
+f=f.read()
+f=f.rstrip("\n")
+if int(f) > int(appcversion):
+  print('UPDATE IS AVALABLE')
+  print('Creating Backup')
+  os.system('cp chk.py chk.py.bk')
+  os.system('rm versionchk')
+  os.system('wget https://raw.githubusercontent.com/mojeg/test/main/versionchk --output-document=chk.py')
+  os.system('python3 chk.py')
+else:
+  print('No updates UTD')
+  os.system('rm versionchk')
